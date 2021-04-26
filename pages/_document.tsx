7ef5cdcddaef3fa173a176d0957d644cbf78c9e2
Pedro Duarte <contact@peduarte.com>
2021-04-26 20:46:04 +0200

    Update font loading strategy

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,6 +3,9 @@ import NextDocument, { Html, Head, Main, NextScript } from 'next/document';
 import { getCssString } from 'stitches.config';
 import { renderSnippet, gtagUrl } from '@lib/analytics';
 
+const FONT_INTER = 'https://fonts.googleapis.com/css?family=Inter:400,500,600,700&display=swap';
+const FONT_FIRA_CODE = 'https://fonts.googleapis.com/css?family=Fira+Mono&display=swap';
+
 export default class Document extends NextDocument {
   render() {
     return (
@@ -12,17 +15,16 @@ export default class Document extends NextDocument {
           <script async src={gtagUrl} />
           <script dangerouslySetInnerHTML={{ __html: renderSnippet() }} />
           <link rel="icon" href="/favicon.ico" />
-          <link
-            href="https://fonts.googleapis.com/css?family=Inter:400,500,600,700&display=swap"
-            rel="preload"
-            as="font"
-            type="font/woff"
-            crossOrigin="anonymous"
-          />
-          <link
-            href="https://fonts.googleapis.com/css?family=Fira+Mono&display=swap"
-            rel="stylesheet"
-          />
+
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+          <link href={FONT_INTER} rel="preload" as="style" />
+          <link href={FONT_INTER} rel="stylesheet" media="all" />
+          <link href={FONT_FIRA_CODE} rel="preload" as="style" />
+          <link href={FONT_FIRA_CODE} rel="stylesheet" />
+          <noscript>
+            <link href={FONT_INTER} rel="stylesheet" />
+            <link href={FONT_FIRA_CODE} rel="stylesheet" />
+          </noscript>
         </Head>
         <body>
           <Main />
