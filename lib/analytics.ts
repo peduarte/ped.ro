8f23287a8c2fbe1d35c42b09ace99568390c265e
Pedro Duarte <contact@peduarte.com>
2021-05-07 19:05:50 +0200

    Add Fathom

--- lib/analytics.ts
+++ lib/analytics.ts
@@ -1,41 +1,22 @@
 import React from 'react';
-import { Router } from 'next/router';
-
-type WindowWithAnalytics = Window &
-  typeof globalThis & {
-    gtag: any;
-  };
-
-const trackingID = 'UA-28314827-1';
+import { useRouter } from 'next/router';
+import * as Fathom from 'fathom-client';
 
 export const useAnalytics = () => {
+  const router = useRouter();
+
   React.useEffect(() => {
-    const handleRouteChange = (url) => {
-      if (process.env.NODE_ENV === 'production') {
-        // We need to wrap it in a rAF to ensure the correct data is sent to Segment
-        // https://github.com/zeit/next.js/issues/6025
-        requestAnimationFrame(() => {
-          (window as WindowWithAnalytics).gtag('config', trackingID, {
-            page_location: url,
-            page_title: document.title,
-          });
-        });
-      }
+    Fathom.load('XPCMEQAE', { includedDomains: ['ped.ro'] });
+
+    function onRouteChangeComplete() {
+      Fathom.trackPageview();
+    }
+    // Record a pageview when route changes
+    router.events.on('routeChangeComplete', onRouteChangeComplete);
+
+    // Unassign event listener
+    return () => {
+      router.events.off('routeChangeComplete', onRouteChangeComplete);
     };
-    Router.events.on('routeChangeComplete', handleRouteChange);
-    return () => Router.events.off('routeChangeComplete', handleRouteChange);
   }, []);
 };
-
-export const gtagUrl = `https://www.googletagmanager.com/gtag/js?id=${trackingID}`;
-
-export function renderSnippet() {
-  if (process.env.NODE_ENV === 'production') {
-    return `
-		window.dataLayer = window.dataLayer || [];
-		function gtag(){dataLayer.push(arguments);}
-		gtag('js', new Date());
-		gtag('config', '${trackingID}');
-		`;
-  }
-}
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import NextDocument, { Html, Head, Main, NextScript } from 'next/document';
 import { getCssString } from 'stitches.config';
-import { renderSnippet, gtagUrl } from '@lib/analytics';
 
 const FONT_INTER = 'https://fonts.googleapis.com/css?family=Inter:400,500,600,700&display=swap';
 const FONT_FIRA_CODE = 'https://fonts.googleapis.com/css?family=Fira+Mono&display=swap';
@@ -12,8 +11,6 @@ export default class Document extends NextDocument {
       <Html lang="en">
         <Head>
           <style id="stitches" dangerouslySetInnerHTML={{ __html: getCssString() }} />
-          <script async src={gtagUrl} />
-          <script dangerouslySetInnerHTML={{ __html: renderSnippet() }} />
           <link rel="icon" href="/favicon.ico" />
 
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
