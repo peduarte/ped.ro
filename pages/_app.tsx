5579e8aa1b1eff2eb208bcc8ab88a8f90ab3faaf
Pedro Duarte <contact@peduarte.com>
2021-05-07 17:28:38 +0200

    Add Plausible

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { AppProps } from 'next/app';
 import Head from 'next/head';
+import PlausibleProvider from 'next-plausible';
 import { IdProvider } from '@radix-ui/react-id';
 import { useAnalytics } from '@lib/analytics';
 import { Footer } from '@components/Footer';
@@ -12,19 +13,21 @@ function App({ Component, pageProps }: AppProps) {
   useAnalytics();
 
   return (
-    <IdProvider>
-      <Head>
-        <title>Pedro Duarte</title>
-      </Head>
+    <PlausibleProvider domain="ped.ro">
+      <IdProvider>
+        <Head>
+          <title>Pedro Duarte</title>
+        </Head>
 
-      <div className={box({ display: 'flex', minHeight: '100vh', flexDirection: 'column' })}>
-        <div className={box({ flex: 1 })}>
-          <Component {...pageProps} />
+        <div className={box({ display: 'flex', minHeight: '100vh', flexDirection: 'column' })}>
+          <div className={box({ flex: 1 })}>
+            <Component {...pageProps} />
+          </div>
+
+          <Footer />
         </div>
-
-        <Footer />
-      </div>
-    </IdProvider>
+      </IdProvider>
+    </PlausibleProvider>
   );
 }
 
