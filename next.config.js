ec0bb0de74c2c5cb4686e6d57dd208c511714709
Pedro Duarte <contact@peduarte.com>
2021-06-22 10:45:02 +0200

    Add social media redirects

--- next.config.js
+++ next.config.js
@@ -8,6 +8,21 @@ const nextConfig = {
 
     return config;
   },
+
+  async redirects() {
+    return [
+      {
+        source: '/twitter',
+        destination: 'https://twitter.com/peduarte',
+        permanent: true,
+      },
+      {
+        source: '/github',
+        destination: 'https://github.com/peduarte',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 module.exports = withVideos(nextConfig);
